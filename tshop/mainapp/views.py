4e415c23b17447096ef319c25ec7b87349290ce7
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2025-05-23 12:52:40 +0530

    edits

--- tshop/mainapp/views.py
+++ tshop/mainapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from .models import Product
 # Create your views here.
 
 def homeView(request):
-    context = {}
+    prods = Product.objects.all() # SELECT * FROM mainapp_product;
+    context = {
+        'products' : prods
+
+    }
     template = 'home.html'
     return render(request, template, context) # this renders the response according to the request using the context
 
